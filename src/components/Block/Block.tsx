e77bf1edc593ed85847032a308e983d47d4bd97d
tomhewitt03 <47860067+tomhewitt03@users.noreply.github.com>
2021-07-28 20:54:28 +0100

    Chromatic Fix

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -18,7 +18,7 @@ const Placeholder = styled(motion.div)`
     display: inline-block;
     padding: 0;
 
-    border-radius: 6px;
+    border-radius: 5px;
 `;
 
 const Container = styled(motion.div)<{ color?: string }>`
