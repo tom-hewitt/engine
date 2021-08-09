39f127796ef51cb69d1334862e92c88e06009be1
tomhewitt03 <47860067+tomhewitt03@users.noreply.github.com>
2021-08-09 20:44:04 +0100

    Update Arrow SVG

--- src/components/BlocksContainer/BlocksContainer.tsx
+++ src/components/BlocksContainer/BlocksContainer.tsx
@@ -14,6 +14,8 @@ const Container = styled(motion.div)`
     display: inline-flex;
     flex-direction: column;
 
+    height: 100%;
+
     padding: 30px;
 
     background: #1E1E1E;
@@ -30,9 +32,20 @@ const ArrowContainer = styled(motion.div)`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    
+`;
+
+const ArrowFixedContainer = styled.div`
+    display: fixed;
+
+    height: 34px;
 `;
 
 const ArrowSVG = styled.svg`
+    display: absolute;
+
+    transform: translate(0px, -7px);
     fill: #919191;
     width: 16px;
     height: 41px;
@@ -43,6 +56,16 @@ function BlocksContainerStart() {
         <StartSVG viewBox="0 0 501 40">
             <path d="M1 1C1 1 1.00337 38.8055 45.0034 38.8096C89.0033 38.8136 410.503 38.781 455.003 38.8014C499.503 38.8218 499.5 1 499.5 1" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"/>
         </StartSVG>
+    )
+}
+
+function Arrow() {
+    return (
+        <ArrowFixedContainer>
+            <ArrowSVG viewBox="0 0 16 41">
+                <path d="M8 0.226497L2.2265 6L8 11.7735L13.7735 6L8 0.226497ZM7.29289 40.7071C7.68342 41.0976 8.31658 41.0976 8.70711 40.7071L15.0711 34.3431C15.4616 33.9526 15.4616 33.3195 15.0711 32.9289C14.6805 32.5384 14.0474 32.5384 13.6569 32.9289L8 38.5858L2.34315 32.9289C1.95262 32.5384 1.31946 32.5384 0.928932 32.9289C0.538408 33.3195 0.538408 33.9526 0.928932 34.3431L7.29289 40.7071ZM7 6V40H9V6H7Z" fill="#919191"/>
+            </ArrowSVG>
+        </ArrowFixedContainer>
     )
 }
 
@@ -58,11 +81,9 @@ function ArrowDroppable(props: { container: BlocksContainerId, index: number }) 
 
     return (
         <ArrowContainer ref={setNodeRef}>
-            <ArrowSVG viewBox="0 0 16 41">
-                <path d="M8 0.226497L2.2265 6L8 11.7735L13.7735 6L8 0.226497ZM7.29289 40.7071C7.68342 41.0976 8.31658 41.0976 8.70711 40.7071L15.0711 34.3431C15.4616 33.9526 15.4616 33.3195 15.0711 32.9289C14.6805 32.5384 14.0474 32.5384 13.6569 32.9289L8 38.5858L2.34315 32.9289C1.95262 32.5384 1.31946 32.5384 0.928932 32.9289C0.538408 33.3195 0.538408 33.9526 0.928932 34.3431L7.29289 40.7071ZM7 6V40H9V6H7Z" fill="#919191"/>
-            </ArrowSVG>
+            <Arrow/>
         </ArrowContainer>
-    )
+    );
 }
 
 function BlockAndArrow(props: { container: BlocksContainerId, id: BlockId, index: number }) {
@@ -79,9 +100,7 @@ function BlockAndArrow(props: { container: BlocksContainerId, id: BlockId, index
         <>
             <Block id={props.id} index={props.index}/>
             <ArrowContainer ref={setNodeRef}>
-                <ArrowSVG viewBox="0 0 16 41">
-                    <path d="M8 0.226497L2.2265 6L8 11.7735L13.7735 6L8 0.226497ZM7.29289 40.7071C7.68342 41.0976 8.31658 41.0976 8.70711 40.7071L15.0711 34.3431C15.4616 33.9526 15.4616 33.3195 15.0711 32.9289C14.6805 32.5384 14.0474 32.5384 13.6569 32.9289L8 38.5858L2.34315 32.9289C1.95262 32.5384 1.31946 32.5384 0.928932 32.9289C0.538408 33.3195 0.538408 33.9526 0.928932 34.3431L7.29289 40.7071ZM7 6V40H9V6H7Z" fill="#919191"/>
-                </ArrowSVG>
+                <Arrow/>
             </ArrowContainer>
         </>
     );
