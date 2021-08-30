03b13294ec151bbbf837e887d41b95bcfd48ec90
tomhewitt03 <47860067+tomhewitt03@users.noreply.github.com>
2021-08-30 19:34:38 +0100

    Fix Global Styles by moving them to index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import { createGlobalStyle } from "styled-components";
 import Editor from "./components/Editor/Editor";
 import reducer from "./reducers/reducer";
 import defaultState from "./defaultState";
-
-createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;700&display=swap');
-`;
 
 export default function App() {
   const store = configureStore({
